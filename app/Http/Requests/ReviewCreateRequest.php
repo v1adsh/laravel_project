a63f1bf2f7a11b14b2bb584da09b3f6fc90860ae
v1adsh <vladshilov20010508@gmail.com>
2021-03-19 13:35:29 +0700

    Review request

--- app/Http/Requests/ReviewCreateRequest.php
+++ app/Http/Requests/ReviewCreateRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class ApplicationCreateRequest extends FormRequest
+class ReviewCreateRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -13,7 +13,7 @@ class ApplicationCreateRequest extends FormRequest
      */
     public function authorize()
     {
-        return true;
+        return false;
     }
 
     /**
@@ -24,14 +24,7 @@ class ApplicationCreateRequest extends FormRequest
     public function rules()
     {
         return [
-
-        ];
-    }
-
-    public function messages()
-    {
-        return [
-
+            //
         ];
     }
 }
